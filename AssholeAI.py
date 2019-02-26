c17575a7785a43e94f82ae2e54bea5c6c9196194
Marcus0108 <38943632+Marcus0108@users.noreply.github.com>
2019-02-26 11:38:40 +0100

    Yea, no errors

--- AssholeAI.py
+++ AssholeAI.py
@@ -1,26 +1,27 @@
-    Ace = 1
-    Two = 2
-    Three = 3
-    Four = 4
-    Five = 5
-    Six = 6
-    Seven = 7
-    Eight = 8
-    Nine = 9
-    Ten = 10
-    Jacks = 11
-    Queens = 1
-    Kings = 13
 
-    Lav = None
-    Middel = None
-    Høj = None
 
-    CurrentCard = None
+Ace = 1
+Two = 2
+Three = 3
+Four = 4
+Five = 5
+Six = 6
+Seven = 7
+Eight = 8
+Nine = 9
+Ten = 10
+Jacks = 11
+Queens = 1
+Kings = 13
 
-    CardsInDeck = None
+Lav = None
+Middel = None
+Høj = None
 
-    Lav < 3
-    Middel >= 4 < 8
-    Høj >= 9 
+CurrentCard = None
+CardsInDeck = None
+
+Lav < 3
+Middel >= 4 < 8
+Høj >= 9 
 
