88d620084d1413f633e6894d4cea16c258c7a2d4
Marcus0108 <38943632+Marcus0108@users.noreply.github.com>
2019-02-26 11:24:41 +0100

    hellnkjsdfåpsdkgnzs

--- AssholeAI.cpp
+++ AssholeAI.cpp
@@ -4,5 +4,29 @@
 using namespace std;
 
 int main(){
-    
+    int Ace = 1;
+    int Two = 2;
+    int Three = 3;
+    int Four = 4;
+    int Five = 5;
+    int Six = 6;
+    int Seven = 7;
+    int Eight = 8;
+    int Nine = 9;
+    int Ten = 10;
+    int Jacks = 11;
+    int Queens = 12;
+    int Kings = 13;
+
+    int Lav;
+    int Middel;
+    int Høj;
+
+    int CurrentCard;
+
+    int CardsInDeck;
+
+    Lav < 3;
+    Middel >= 4 < 8;
+    Høj >= 9; 
 }
