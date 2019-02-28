d95990031832e3e26877a5bec5a4d9838879582d
Marcus0108 <38943632+Marcus0108@users.noreply.github.com>
2019-02-28 14:21:05 +0100

    Create AssholeAI.py

---
+++ Python/AssholeAI.py
@@ -0,0 +1,26 @@
+
+Ace = 1
+Two = 2
+Three = 3
+Four = 4
+Five = 5
+Six = 6
+Seven = 7
+Eight = 8
+Nine = 9
+Ten = 10
+Jacks = 11
+Queens = 1
+Kings = 13
+
+Lav = None
+Middel = None
+Høj = None
+
+CurrentCard = None
+CardsInDeck = None
+
+Lav < 3
+Middel >= 4 < 8
+Høj >= 9 
+
